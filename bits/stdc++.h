990e944d4a656634e7d06cf7f636182ae1d7f8ae
Anurag5 <anuraghere555@gmail.com>
2025-10-25 19:02:23 +0530

    Add files via upload

---
+++ bits/stdc++.h
@@ -0,0 +1,19 @@
+#ifndef _BITS_STDCPP_H
+#define _BITS_STDCPP_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+#endif
