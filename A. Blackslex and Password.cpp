55fb00e6cb7e64cd0cf5d978934f64969b02c9a3
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-12 21:32:00 +0530

    Add files via upload

---
+++ A. Blackslex and Password.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+typedef long long ll;
+
+void result()
+{
+   int a , b;
+   cin>>a>>b;
+   cout<< (a * b)+1 <<endl;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        result();
+    }
+    return 0;
+}
