72374fcd46fbad283c154ec19258ffca07e2cbf4
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-14 23:14:23 +0530

    Add files via upload

---
+++ D_Yet_Another_Array_Problem.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+typedef long long ll;
+
+void result()
+{
+   int n;
+   cin>>n;
+   vector<int> a(n);
+   for(int i =0;i<n;i++) cin>>a[i];
+
+   
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        result();
+    }
+    return 0;
+}
