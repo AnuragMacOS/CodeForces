bc7a29c2117674fbf9c7234b1212d44cc4189f1f
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-17 23:46:05 +0530

    Add files via upload

---
+++ B_osu_mania.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define forn(i,n)  for(int i=0;i<n;i++)
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<string> s(n);
+
+    for(int i =0;i<n;i++){
+        cin>>s[i];
+    }
+     
+    for(int i =n-1;i>=0;i--){
+        for(int j=0;j<4;j++){
+           if(s[i][j]=='#'){
+            cout<< j + 1 <<' ';
+           } 
+        } 
+    }
+     cout<<endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
