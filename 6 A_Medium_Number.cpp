21435d26e9b0ae50b73cea62fffde325e16231d6
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-24 22:53:56 +0530

    Add files via upload

---
+++ 6 A_Medium_Number.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+
+void solve() {
+    vector<int> a(3);
+    cin>>a[0]>>a[1]>>a[2];
+
+    sort(all(a));
+    cout<<a[1]<<"\n";
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
