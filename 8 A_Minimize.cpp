1f519b0ad45df82bef83fab7e1878d559b91f7db
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-24 22:54:18 +0530

    Add files via upload

---
+++ 8 A_Minimize.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+
+void solve() {
+    int a ,b;
+    cin>>a>>b;
+   
+   cout<<b-a<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
