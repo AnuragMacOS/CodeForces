f40ddce6707bdeef785a8d749cd268cca35e0d12
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-25 22:42:57 +0530

    Add files via upload

---
+++ 10 A_Odd_One_Out.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+
+void solve() {
+    int a , b ,c;
+    cin>>a>>b>>c;
+    
+    if(a==b && a!=c) cout<<c<<endl;
+    if(a==c && a!=b) cout<<b<<endl;
+    if(b==c && b!=a) cout<<a<<endl;
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
