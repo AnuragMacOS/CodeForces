4d5fcdae6090402a63bb53ea6d81dc1acffe0a8f
Anurag5 <anurag_23it183@dtu.ac.in>
2026-01-31 20:51:20 +0530

    Add files via upload

---
+++ 32 A. Perfect Root.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define forn(i,n)  for(int i=0;i<n;i++)
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << i;
+        if (i < n) {
+            cout << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
