be489105961fd56fdb0317fbe69803fdd7caefa5
Anurag5 <anurag_23it183@dtu.ac.in>
2026-02-11 15:01:54 +0530

    Add files via upload

---
+++ A. Game with Integers.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define forn(i,n)  for(int i=0;i<n;i++)
+
+void solve() {
+    int n;
+    cin>>n;
+
+    if(n % 3 == 0) cout<<"Second"<<endl;
+    else cout<<"First"<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
