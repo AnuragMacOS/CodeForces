f1155b58841368166e4cc7c0a9c607658d1a8624
Anurag5 <anurag_23it183@dtu.ac.in>
2026-02-16 23:17:05 +0530

    Add files via upload

---
+++ C. Target Practice.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define forn(i, n) for (int i = 0; i < n; i++)
+
+const int score[10][10] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+void solve()
+{
+    char a[10][10];
+    for (int i = 0; i < 10; i++)
+    {
+        string s;
+        cin >> s;
+        for (int j = 0; j < 10; j++)
+        {
+            a[i][j] = s[j];
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (a[i][j] == 'X')
+            {
+                ans += score[i][j];
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
