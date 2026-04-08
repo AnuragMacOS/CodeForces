ecd30223d3b0d17c3b6166d59dd5554a2583fee6
Anurag5 <anurag_23it183@dtu.ac.in>
2026-04-08 22:42:53 +0530

    Add files via upload

---
+++ A. Coins.cpp
@@ -0,0 +1,41 @@
+#define _Alignof alignof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define forn(i, n) for (int i = 0; i < n; i++)
+
+void solve()
+{
+  ll n, k;
+  cin >> n >> k;
+
+  if (n % 2 == 0 || (n - k) % 2 == 0)
+  {
+    cout << "YES" << endl;
+  }
+  else
+    cout << "NO" << endl;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
