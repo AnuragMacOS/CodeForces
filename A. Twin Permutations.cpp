5e1b59d734c9e34d41d5bddd25065617b746b9b9
Anurag5 <anurag_23it183@dtu.ac.in>
2026-04-08 22:43:05 +0530

    Add files via upload

---
+++ A. Twin Permutations.cpp
@@ -0,0 +1,40 @@
+#define _Alignof alignof
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define forn(i,n)  for(int i=0;i<n;i++)
+
+void solve() {
+    int n;
+    cin>>n;
+    ll a[n];
+    forn(i,n){
+        cin>>a[i];
+    }
+    forn(i,n){
+        cout<< n + 1 - a[i] << " ";
+    }
+    cout<<endl;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
